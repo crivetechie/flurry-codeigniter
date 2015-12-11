699c07d5f81cf89655df1aa81aa64245d058eff5
Marco Crivellaro <marco.crive@gmail.com>
2015-12-11 14:13:22 +0000

    cleanup of the controller

--- htdocs/application/controllers/Flurry.php
+++ htdocs/application/controllers/Flurry.php
@@ -1,31 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-/*
-require_once __DIR__.'/../libraries/Thrift/ClassLoader/ThriftClassLoader.php';
-require_once __DIR__.'/../libraries/Thrift/Protocol/TProtocol.php';
-require_once __DIR__.'/../libraries/Thrift/Protocol/TBinaryProtocol.php';
-require_once __DIR__.'/../libraries/Thrift/Transport/TTransport.php';
-require_once __DIR__.'/../libraries/Thrift/Transport/TSocket.php';
-require_once __DIR__.'/../libraries/Thrift/Transport/THttpClient.php';
-require_once __DIR__.'/../libraries/Thrift/Transport/TBufferedTransport.php';
-require_once __DIR__.'/../libraries/Thrift/Exception/TException.php';
-require_once __DIR__.'/../libraries/Thrift/Type/TMessageType.php';
-require_once __DIR__.'/../libraries/Thrift/Factory/TStringFuncFactory.php';
-require_once __DIR__.'/../libraries/Thrift/StringFunc/Core.php';
-require_once __DIR__.'/../libraries/Thrift/StringFunc/TStringFunc.php';
-*/
 require_once __DIR__.'/../libraries/Bazu/Flurry/Flurry.php';
 require_once __DIR__.'/../libraries/Bazu/Flurry/Types.php';
-
-/*
-use \Thrift\ClassLoader\ThriftClassLoader;
-use \Thrift\Protocol\TBinaryProtocol;
-use \Thrift\Transport\TSocket;
-use \Thrift\Transport\THttpClient;
-use \Thrift\Transport\TBufferedTransport;
-use \Thrift\Exception\TException;
-*/
-
 
 class Flurry extends CI_Controller {
 
@@ -40,17 +16,8 @@ class Flurry extends CI_Controller {
     $this->loader->registerDefinition('shared', $gen_dir);
     $this->loader->registerDefinition('tutorial', $gen_dir);
     $this->loader->register();    
-
  
   }
-
-  /*
-  use Thrift\Protocol\TBinaryProtocol;
-  use Thrift\Transport\TSocket;
-  use Thrift\Transport\THttpClient;
-  use Thrift\Transport\TBufferedTransport;
-  use Thrift\Exception\TException;
-  */
 
   public function index() {
     try {
